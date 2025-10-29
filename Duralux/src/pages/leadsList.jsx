20e2c608e37285ca38f88d900643d383ae888a00
Mohammed Fais TM <mhdfaistm@gmail.com>
2025-10-29 15:50:10 +0530

    lead list again created

--- Duralux/src/pages/leadsList.jsx
+++ Duralux/src/pages/leadsList.jsx
@@ -1,14 +1,13 @@
-import LeadsTable from '@/components/leads/LeadsTable'
-import React from 'react'
+import React from "react";
 
 const LeadsList = () => {
   return (
     <div className="main-content">
-        <div className="row">
-          <LeadsTable />
-        </div>
+      <div className="row">
+        <LeadsTable />
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default LeadsList
+export default LeadsList;
